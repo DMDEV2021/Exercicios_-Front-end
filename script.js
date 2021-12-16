b548e3cc353518ae0cac8da88190d04d96044f9a
DMDEV2021 <89950908+DMDEV2021@users.noreply.github.com>
2021-12-16 20:34:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+/* <label class="todo_item">
+             <input type="checkbox">
+             <div>Teste de item 1</div>
+             <input type="button" value="+">
+</label> */
+/*
+const criarItem = () => {
+    const item = document.createdElement('label');
+    item.classList.add()
+}
+*/
